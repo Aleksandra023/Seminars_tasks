c79ee4697a04441edc63a0d962a298909aba4bc1
Aleksandra <toropova230@gmail.com>
2022-12-23 16:37:42 +0300

    добавлена задача №47

---
+++ seminar_07/47/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами от -10,0 до 10,0.
+// m = 3, n = 4.
+// 0,5 7,0 -2,0 -0,2
+// 1,0 -3,3 8,0 -9,9
+// 8,0 7,8 -7,1 9,0
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] twoDimArray = new double[m, n];
+Random rnd = new Random();
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+ Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);
+        }
+    }
+}
+
+FillArray(twoDimArray);
+Console.WriteLine();
+PrintArray(twoDimArray);
